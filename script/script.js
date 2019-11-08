8e950e7b3ebc3f1ce75d8e45ecc8934fd054d195
Anatoliy <toliankir@gmail.com>
2019-11-08 22:12:54 +0200

    Add TaskObject event hanler, add images event handlers.

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
-const completedTaskCounterElement = document.querySelector('#completed-task');
-const openTaskCounterElement = document.querySelector('#open-task');
+const notifyMassage = 'Are you sure you want to change the number of tasks?';
 
-completedTaskCounterElement.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    completedTaskCounterElement.innerText = parseInt(completedTaskCounterElement.innerText) + 1;
-    // completedTaskCounterElement.innerText = parseInt(completedTaskCounterElement.innerText)++;
+    const completedTaskCounterElement = document.querySelector('#completed-task');
+    const openTaskCounterElement = document.querySelector('#open-task');
+    const bottomImagesElements = document.querySelectorAll('.images img');
+    const notifyElement = document.querySelector('#notify');
+
+    completedTaskCounterElement.addEventListener('click', () => {
+        if (!confirm(notifyMassage)) {
+            return;
+        }
+        
+        if (openTaskCounterElement.innerText !== '0') {
+            openTaskCounterElement.innerText = parseInt(openTaskCounterElement.innerText) - 1;
+        }
+        completedTaskCounterElement.innerText = parseInt(completedTaskCounterElement.innerText) + 1;
+    });
+
+    bottomImagesElements.forEach((el, index) =>
+        el.addEventListener('click', () => { notifyElement.innerText = index }));
 });
-
-// console.log(completedTaskCounterElement);
-// completedTaskCounterElement.innerText = 123;
