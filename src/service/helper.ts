8d8955a6d9c9510074d458388cae1506844a60f4
Anatoliy <toliankir@gmail.com>
2020-01-27 09:11:31 +0200

    Change floor to trunc... 	modified:   src/service/helper.ts

--- src/service/helper.ts
+++ src/service/helper.ts
@@ -1,4 +1,4 @@
-export const getUnixTimeStamp = (): string => Math.floor(Date.now() / 1000).toString();
+export const getUnixTimeStamp = (): string => Math.trunc(Date.now() / 1000).toString();
 
 export const timestampToDate = (timestamp: string):string => {
   const date = new Date(parseInt(timestamp, 10) * 1000);
